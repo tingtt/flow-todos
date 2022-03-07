7a24f8e941a4c5a8b1cebbb966e9f4becd2e265e
tingtt <ting.taku@gmail.com>
2022-03-07 18:26:37 +0900

    [fix] Todo.Idのjsonキーを指定

--- todo/todo.go
+++ todo/todo.go
@@ -9,7 +9,7 @@ import (
 )
 
 type Todo struct {
-	Id            uint64
+	Id            uint64  `json:"id"`
 	Name          string  `json:"name"`
 	Description   *string `json:"description,omitempty"`
 	Date          *string `json:"date,omitempty"`
