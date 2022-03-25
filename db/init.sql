b0e41701886722028b5c096f1def8e08b294c4f8
tingtt <ting.taku@gmail.com>
2022-03-25 13:42:37 +0900

    [clean] 予約語を大文字に

--- db/init.sql
+++ db/init.sql
@@ -16,16 +16,16 @@ USE `flow-todos`;
 --
 
 CREATE TABLE `todos` (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  `time` time DEFAULT NULL,
-  `execution_time` int DEFAULT NULL COMMENT 'minute',
-  `sprint_id` bigint UNSIGNED DEFAULT NULL,
-  `project_id` bigint UNSIGNED DEFAULT NULL,
-  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT UNSIGNED NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `description` VARCHAR(255) DEFAULT NULL,
+  `date` DATE DEFAULT NULL,
+  `time` TIME DEFAULT NULL,
+  `execution_time` INT DEFAULT NULL COMMENT 'minute',
+  `sprint_id` BIGINT UNSIGNED DEFAULT NULL,
+  `project_id` BIGINT UNSIGNED DEFAULT NULL,
+  `completed` TINYINT(1) NOT NULL DEFAULT '0',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
